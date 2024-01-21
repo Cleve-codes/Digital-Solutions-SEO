5b440fe699f0a1d68d63321db64d1607f72619ce
Cleve-codes <clevemomanyi@gmail.com>
2024-01-21 18:43:10 +0300

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  Menu from './components/Menu/Menu';
+import Hero from './sections/Hero/Hero';
+
 
 function App() {
 
   return (
     <div className='container'>
       <Menu/>
+      <Hero/>
     </div>
   )
 }
