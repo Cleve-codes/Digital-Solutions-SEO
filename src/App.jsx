8848da423584f834bf881efc7c3dedd0ea97aa76
Cleve-codes <clevemomanyi@gmail.com>
2024-01-21 19:22:55 +0300

    process

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './sections/Hero/Hero';
 import Services from './sections/services/Services';
 import CTA from './sections/CTA/CTA';
 import CaseStudies from './sections/CaseStudies/CaseStudies';
+import Process from './sections/Process/Process';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Services/>
       <CTA/>
       <CaseStudies />
+      <Process />
     </div>
   )
 }
