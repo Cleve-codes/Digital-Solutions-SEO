ac368d8353d18e9d7d761bfaffee4c77a050ddc2
Cleve-codes <clevemomanyi@gmail.com>
2024-01-23 05:44:27 +0300

    footer done

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,15 +1,14 @@
-// import styles from "../style";
-// import { logo } from "../assets";
 import { data } from '../../constants'
 import { images } from '../../constants'
+import './Footer.css'
 
 const Footer = () => (
-  <section className='section'>
-    <div className='container'>
+  <section className='footer__section'>
+    <div className='footer__container'>
       <div className="logo">
       <div style={{ display: 'flex', alignItems: 'center', gap: '1em' }}>
         <img src={images.logo} alt="logo" className='img-logo'  />
-        <h1 className="h1">SM Digital</h1>
+        <h1 className="h1">SMS Digital</h1>
       </div>
         <p>
           A new way to make the payments easy, reliable and secure.
@@ -18,17 +17,17 @@ const Footer = () => (
 
       <div className="links">
         {data.FooterLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+          <div key={footerlink.title}
+          >
+            <h4
+            >
               {footerlink.title}
             </h4>
-            <ul className="list-none mt-4">
+            <ul
+            >
               {footerlink.links.map((link, index) => (
                 <li
                   key={index}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
                 >
                   {link.name}
                 </li>
@@ -39,32 +38,28 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+
+    <div
+    className='hr'
+    ></div>
+
+    {/* Footer Bottom */}
+
+    <div className='footer__foot'>
+      <div className='right'>
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2024 Stats Marketing Services. All Rights Reserved.
+        Copyright Ⓒ 2024 SMS. All Rights Reserved.
       </p>
+      </div>
 
       {/* Social Media Icons */}
-      <div className="flex flex-row md:mt-0 mt-6">
-        {/* {data.socialMedia.map((social, index) => (
-          <img
-            key={social.id}
-            src={social.icon}
-            alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-              index !== data.socialMedia.length - 1 ? "mr-6" : "mr-0"
-            }`}
-            onClick={() => window.open(social.link)}
-          />
-        ))} */}
+      <div className="footer__social-links">
         {
           data.socialMedia.map((social, index) => {
             return (
             <a key={index} href={social.link} >
               <social.icon
-              className={`w-[21px] h-[21px] object-contain cursor-pointer text-white ${
-                index !== data.socialMedia.length - 1 ? "mr-6" : "mr-0"
-              }`}
+              className='icon'
               onClick={() => window.open(social.link)}
                />
               </a>
